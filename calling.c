000f1a60e9cf3babf594b49be32580a70e5a8116
Roger Binns <rogerb@rogerbinns.com>
2023-08-12 08:54:21 -0700

    Update C and Python code

--- calling.c
+++ calling.c
@@ -13,7 +13,7 @@ with_varargs_keywords(PyObject *self,
                       PyObject *args,
                       PyObject *kwargs)
 {
-    return Py_BuildValue("OO", OBJ(args), OBJ(kwargs));
+    return Py_BuildValue("{s:O,s:O}", "args", OBJ(args), "kwargs", OBJ(kwargs));
 }
 
 static PyObject *
@@ -22,19 +22,40 @@ with_fastcall_keywords(PyObject *self,
                        Py_ssize_t nargs,
                        PyObject *kwnames)
 {
-    nargs += 3;
-    PyObject *t = PyTuple_New(nargs);
-    for (Py_ssize_t i = 0; i < nargs; i++)
+    Py_ssize_t actual_nargs = nargs;
+    if (kwnames)
+        actual_nargs += PyTuple_Size(kwnames);
+    PyObject *t = PyTuple_New(actual_nargs);
+    for (Py_ssize_t i = 0; i < actual_nargs; i++)
     {
         Py_INCREF(args[i]);
         PyTuple_SetItem(t, i, args[i]);
     }
-    return Py_BuildValue("NO", t, OBJ(kwnames));
+    return Py_BuildValue("{s:n,s:N,s:O}", "nargs", nargs, "args", t, "kwnames", OBJ(kwnames));
+}
+
+static PyObject *
+bench_varargs_keywords(PyObject *self,
+                       PyObject *args,
+                       PyObject *kwargs)
+{
+    Py_RETURN_NONE;
+}
+
+static PyObject *
+bench_fastcall_keywords(PyObject *self,
+                        PyObject *const *args,
+                        Py_ssize_t nargs,
+                        PyObject *kwnames)
+{
+    Py_RETURN_NONE;
 }
 
 static PyMethodDef methods[] = {
     {"with_varargs_keywords", (PyCFunction)with_varargs_keywords, METH_VARARGS | METH_KEYWORDS, NULL},
     {"with_fastcall_keywords", (PyCFunction)with_fastcall_keywords, METH_FASTCALL | METH_KEYWORDS, NULL},
+    {"bench_varargs_keywords", (PyCFunction)bench_varargs_keywords, METH_VARARGS | METH_KEYWORDS, NULL},
+    {"bench_fastcall_keywords", (PyCFunction)bench_fastcall_keywords, METH_FASTCALL | METH_KEYWORDS, NULL},
     {0, 0, 0, 0}};
 
 static struct PyModuleDef calling_module = {PyModuleDef_HEAD_INIT, "calling", NULL, -1, methods};
--- investigate.py
+++ investigate.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python
 
 import pprint
+import timeit
 
 import calling # python3 setup.py build_ext --inplace --force
 
+print("Calling convention for meth(1, 2, 3, four=4, five=5, six=6)")
 for n in dir(calling):
     if n.startswith("with_"):
         v=getattr(calling, n)(1, 2, 3, four=4, five=5, six=6)
         print(n)
         pprint.pprint(v)
+
+print("Duplicate keyword args")
+for n in dir(calling):
+    if n.startswith("with_"):
+        print(n)
+        try:
+            v=getattr(calling, n)(1, 2, 3, four=4, five=5, **{"four": 6})
+            pprint.pprint(v)
+        except TypeError:
+            print("  TypeError")
+
+print("Calling overhead method(1, 2, 3, four=4, five=5, six=6)")
+for n in dir(calling):
+    if n.startswith("bench_"):
+        print(n)
+        t=timeit.Timer("method(1, 2, 3, four=4, five=5, six=6)", setup=f"method=getattr(calling, '{ n }')", globals={"calling": calling})
+        nloops, _ = t.autorange()
+        nloops *=5 # run for ~1 second
+        took=t.timeit(nloops)
+        print(f"{ 1e9 * took / nloops:.1f} nanoseconds per call")
+
